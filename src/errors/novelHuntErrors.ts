8cf24364c704705a36fc5fbc7347f13f6b91c0ff
MarianaKely <marianakely.r@gmail.com>
2023-06-30 02:30:12 -0300

    feat: errors

---
+++ src/errors/novelHuntErrors.ts
@@ -0,0 +1,74 @@
+
+ function loginError(): Error {
+
+    return {
+
+      name: "LoginError",
+      message: "Create a account",
+
+    };
+
+  }
+
+
+   function invalidEmail (email: string): Error {
+
+    return {
+
+      name: "invalidEmail",
+      message: `invalid Emaill: ${email}`,
+
+    };
+
+  }
+
+
+  function ErrorInformations(message?: string): Error {
+
+    return {
+
+      name: "ErrorInformations",
+      message: message || "Invalid email or password  ",
+
+    };
+
+  }
+
+
+   function InternalError(message: string): Error {
+
+    return {
+
+      name: "InternalError",
+      message,
+
+    };
+
+  }
+
+
+
+   function dateError(message: string): Error {
+
+    return {
+
+      name: "DateError",
+      message,
+
+    };
+
+  }
+ 
+   function notFoundError(message: string): Error {
+
+    return {
+
+      name: "NotFoundError",
+      message,
+
+    };
+
+  }
+
+
+export default { notFoundError , dateError, loginError, invalidEmail, InternalError, ErrorInformations}
